4271dca4e746494d860599a4b3e80ac0a3769dcd
Matt Weidner <mweidner44@gmail.com>
2019-10-14 20:11:19 +0900

    Added a final test, to test larger strings

--- words_frequency/spec.js
+++ words_frequency/spec.js
@@ -31,4 +31,14 @@ describe('word frequency', () => {
         expect(topThreeWords(testCase)).to.deep.equal([])
         expect(topThreeWords(testCase2)).to.deep.equal([])
     })
+    it("should return the correct word frequency regardless of string length", () => {
+        const testCase = `In a village of La Mancha, the name of which I have no desire to call to
+        mind, there lived not long since one of those gentlemen that keep a lance
+        in the lance-rack, an old buckler, a lean hack, and a greyhound for
+        coursing. An olla of rather more beef than mutton, a salad on most
+        nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+        on Sundays, made away with three-quarters of his income.`
+
+        expect(topThreeWords(testCase)).to.deep.equal(['a','of','on'])
+    })
 })
