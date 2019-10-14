d8481b1f1cf9bd9e68f51eca30e4e3b8eebb9b5b
Matt Weidner <mweidner44@gmail.com>
2019-10-14 19:41:26 +0900

    Passing a 3rd test!

--- words_frequency/words.js
+++ words_frequency/words.js
@@ -8,28 +8,52 @@
 
 
 function topThreeWords(text) {
-    let textArray = text.split(" ")
-    let answer = []
+    let textArray = text.split(" ").filter(word => {
+        return word.length > 0
+    })
     let memo = {}
+    let answer = []
 
+    const isAlpha = ch => {
+        return ch.match(/^[a-z']+$/i) !== null;
+    }
+
+    let wordArray;
+    textArray.forEach((word, index) => {
+        wordArray = word.split('')
+        for (let i = 0; i < wordArray.length; i++) {
+            if(!isAlpha(wordArray[i])) {
+                wordArray.splice(i, 1)
+                i -= 1
+            }
+        }
+        textArray[index] = wordArray.join('')
+    })
+    
+    console.log(textArray, 'after symbol check at each index')
     textArray.forEach(word => {
         let current = word.toLowerCase()
 
-        if (!memo[current]) {
+        if (!memo[current] && isAlpha(word)) {
             memo[current] = 1
-        }else if (memo[current]) {
+        }else if (memo[current] && isAlpha(word)) {
             memo[current] += 1
         }
     })
 
+    console.log(memo, 'current word count')
+
     const memoKeys = Object.keys(memo)
-    const isAlpha = ch => {
-        return ch.match(/^[a-z]+$/i) !== null;
-    }
 
     memoKeys.forEach(key => {
         if (answer.length < 1 && isAlpha(key)) {
             answer.push(key)
+        } 
+        else if (memo[key] < memo[answer[0]]) {
+            answer.push(key)
+        }
+        else {
+            answer.unshift(key)
         }
     })
     console.log(answer)
