8f939c43f416d687451d8f21b0c702749fabac17
Matt Weidner <mweidner44@gmail.com>
2019-10-29 18:06:05 +0900

    Edited test description

--- tree_reconstruct/spec.js
+++ tree_reconstruct/spec.js
@@ -2,7 +2,7 @@ const { expect } = require("chai")
 const { reconstructTree } = require("./tree")
 
 describe("Tree Reconstruction", () => {
-    it("example test", () => {
+    it("Should return the original binary tree given in-order and pre-order arrays", () => {
       expect(reconstructTree([1,2,3,4,5,6],[4,2,1,3,6,5])).to.deep.equal([4,[2,[1,[],[]],[3,[],[]]],[6,[5,[],[]],[]]])
     });
   }); 
