fb62eeb820d16a41fc253e681290fa98d1396179
Matt Weidner <mweidner44@gmail.com>
2019-10-29 18:57:18 +0900

    Co-authored-by: Wil Andrade <andradewilker@gmail.com>

--- tree_reconstruct/tree.js
+++ tree_reconstruct/tree.js
@@ -1,5 +1,15 @@
 function reconstructTree(inOrder, preOrder) {
+
     let answer = [];
+
+    // const createTree = (curNode, remainingVals) => {
+    //     if(remainingVals.length < 1) return;
+    //     const [head, ...others] = remainingVals.slice(0);
+    //     head.push([]); //left
+    //     head.push([]); //right
+    //     curNode.push(head);
+    // };
+    // createTree(answer, preOrder.slice());
 
     const recurse = (index = 0) => {
 
@@ -20,3 +30,25 @@ function reconstructTree(inOrder, preOrder) {
 
 
 module.exports = { reconstructTree };
+
+// this is the tree to reconstruct:
+//        4
+//       / \
+//      /   \
+//     2     6
+//    / \   /
+//   1   3 5
+
+// inOrder  = [1,2,3,4,5,6]
+// preOrder = [4,2,1,3,6,5]
+
+// reconstructTree(inOrder,preOrder) => [ 4
+//                                      , [ 2
+//                                        , [ 1 , [], [] ]
+//                                        , [ 3 , [], [] ]
+//                                        ]
+//                                      , [ 6
+//                                        , [ 5 , [], [] ]
+//                                        , []
+//                                        ]
+//                                      ]
